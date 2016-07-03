ee0fc491018f6e100e6107ab8fd4a43ca1d169d8
André Herculano <andresilveirah@gmail.com>
2016-07-03 16:30:12 +0200

    add market_adapters

--- lib/market_adapters/talon_ro.rb
+++ lib/market_adapters/talon_ro.rb
@@ -1,5 +1,7 @@
 # Offer model
 # t.string :item_name
+# t.integer :slots
+# t.integer :refinement
 # t.string :cards
 # t.integer :price
 # t.integer :amount
@@ -10,17 +12,21 @@
 module MarketAdapters
   class TalonRO
     def initialize offer
-      {
+      @offer_hash = {
         item_name:    offer.fetch(:item_name),
         refinement:   offer.fetch(:refinement, nil),
         slots:        offer.fetch(:slots, 0),
-        cards:        offer.fetch(:cards, nil),
+        cards:        offer.fetch(:cards, nil).try(:join, ' '),
         price:        offer.fetch(:price),
         amount:       offer.fetch(:amount),
         shop_title:   offer.fetch(:shop_title),
         vendor:       offer.fetch(:vendor),
         map_location: offer.fetch(:coords, nil)
       }
+    end
+
+    def to_h
+      @offer_hash
     end
   end
 end
