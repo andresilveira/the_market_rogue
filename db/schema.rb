9e91a941d4cb3bfe06feccc6dcaae15dfe05e51c
André Herculano <andresilveirah@gmail.com>
2016-07-12 15:49:06 +0200

    add track_sellers attribute to Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709094416) do
+ActiveRecord::Schema.define(version: 20160712121143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.boolean  "track_sellers"
   end
 
   create_table "markets", force: :cascade do |t|
@@ -40,7 +41,9 @@ ActiveRecord::Schema.define(version: 20160709094416) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "item_id"
+    t.string   "type"
     t.index ["item_id"], name: "index_offers_on_item_id", using: :btree
+    t.index ["type"], name: "index_offers_on_type", using: :btree
   end
 
 end
